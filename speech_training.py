8e63736e7336fa99f1c0ed12670d1d12ad4be0ca
Jon Brennecke <jpbrennecke@gmail.com>
2013-09-07 16:55:37 -0700

    Create speech_training.py

---
+++ speech_training.py
@@ -0,0 +1,53 @@
+from __future__ import division
+import os, wave, struct
+import signallib
+import numpy as np
+import ann
+
+
+if __name__ == '__main__':
+
+	index = 0
+	chunk = 256
+	n = ann.Network(14,30)
+	mp = ann.SelfOrganizingMap(n) 
+	# n.createFile()
+
+	try :
+		files = os.listdir("../wav")
+		for filename in files :
+			try :
+				wav = wave.open("../wav/" + filename)
+				bin = wav.readframes(wav.getnframes())
+				data = []
+				for i in range(0,len(bin)) :
+					if i%2 != 0 :
+						continue
+
+					# convert the .wav to decimal
+					data.append(struct.unpack("<h", bin[i:i+2])[0])
+
+				rate,chunk = (wav.getframerate(),256)
+				duration = (len(data)/rate)*1000 # duration of stream in ms
+
+				# feature vectors are computed every 10ms
+				# in an overlapping analysis window of 30ms
+				featureVector = []
+				for i in range(0,int(duration/10)) :
+					frame = data[i*10:(i*10)+30]
+					mfcc = signallib.mfcc(frame)
+					output = n.update(mfcc)
+					mp.update(mfcc[0:len(mfcc)-1],index,len(files)*3)
+					index += 1
+
+				name = filename.split('.')[0]
+				print name
+				# f = open('../lib/' + name + '.log', 'w')
+				# f.write(str(mfcc))
+				# f.close()
+
+			except IOError:
+				pass
+
+	except OSError :
+		pass
